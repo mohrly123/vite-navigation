bd13bf12f156f58f37fa466c8524d5a1d1f4c102
mohrly123 <168989632+mohrly123@users.noreply.github.com>
2024-11-26 12:43:13 +0100

    ,

--- src/Seite3/seitedrei.jsx
+++ src/Seite3/seitedrei.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Third(){
     return(
         <Link to="/">
-            <h1>Seite 3adsfsvfsvs</h1>
+            <h1>Ich bin Seite 3 du lauch</h1>
             <button type="button">Zur Main Page</button>
         </Link>
     );
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      },
+    },
+  },
   base: "/vite-navigation/", // YOUR REPO NAME HERE
 })
